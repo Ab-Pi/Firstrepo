410a6181877709fb54b859ce03262873f00bbb5a
AbPI <pizoo.abdoo8888@gmail.com>
2022-07-02 20:28:39 +0200

    install django-heroku and update settings

--- MedDatabase/settings.py
+++ MedDatabase/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,7 +143,11 @@ STATICFILES_DIRS =(
 
 STATIC_DIR,
 )
+
 django_heroku.settings(locals())
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
